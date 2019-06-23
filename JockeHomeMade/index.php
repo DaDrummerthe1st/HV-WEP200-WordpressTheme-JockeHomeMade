378337b87d685bb5aeaa3266160a864788518188
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2019-06-23 10:12:34 +0200

    continued working on the loop reg posts on index.php

--- JockeHomeMade/index.php
+++ JockeHomeMade/index.php
@@ -10,11 +10,57 @@
 </div><!-- /carousel-small -->
 
 <div id="content">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    
-    <?php endwhile; else: ?>
+<?php
+    # https://codex.wordpress.org/The_Loop 
+    # Not really written the same way here, but this way doesnt render an error
+?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <article class="post">
 
-    <?php endif; ?>
+        <header class="post">
+
+            <section class="entry-title">
+                <h2>
+                    <a href="<?php
+                                the_permalink();
+                                ?>"
+                        title="<?php
+                                the_title_attribute();
+                                ?>"
+                        rel="bookmark"><?php
+                            the_title();
+                    ?></a>
+                </h2>
+            </section><!-- /entry-title -->
+
+            <section class="entry-meta">
+                <p>
+                Written by <?php the_author_posts_link(); ?>
+                on <?php the_time('j F Y'); ?>
+                in <?php the_category(', '); # In order to later sort different kinds of posts, use get_category_ID() ?>.
+                    <?php
+                        if(comments_open()) {
+                            # comments-link is defined in wp
+                            echo '<span class="comments-link"><a href="' . get_comments_link() . '">Comment!</a></span>';
+                        } else {
+                            echo ' Not possible to comment!';
+                        }
+                    ?>
+                </p>
+            </section><!-- /entry-meta -->
+
+        </header><!-- /post -->
+        
+        <section class="entry-content">
+        </section><!-- /entry-content -->
+
+        <footer class="post">
+        </footer><!-- /post -->
+
+    </article><!-- /post -->
+<?php endwhile; else: ?>
+
+<?php endif; ?>
 </div><!-- /content -->
 
 <?php get_sidebar(); ?>
