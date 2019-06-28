c283790b2715bafda9f303373c98577645f59aa5
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2019-06-28 15:29:55 +0200

    added responsive design plus comments minus an issue on landing page

--- JockeHomeMade/header.php
+++ JockeHomeMade/header.php
@@ -9,6 +9,20 @@
     <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/b30a91c97e.js"></script> <!-- from Font Awesome login page -->
     
+    <!--
+    disables the possibility to pinch zoom 
+    initial-scale=1 == när man kommer till sidan är den inzoomad till 100%
+    maximum-scale=1 == det mesta man kan zooma in på sidan är 100% aka man kan inte pinch zooma
+    -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+    <!-- 
+        IE < 9 can not handle media queries, thus the following special hack
+        If less than IE 9
+        The Google script makes older web browser responsive
+    -->
+    <!-- [if lt IE 9]>
+        <script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <! [endif]-->
     <?php wp_head(); # tells wp the header ends and is possible to add more material ?>
 </head>
 
